4f29f6026b19f368512cd5f13bdaaa17a6cde0b4
rainbowhxch <rainbowhxch@gmail.com>
2020-11-11 00:08:53 +0800

    db-file: now buffer can sync to .db file

--- src/rainbow-db.cpp
+++ src/rainbow-db.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdint-uintn.h>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <cstdint>
 #include <cstring>
-#include <sys/types.h>
 
 using std::cin;
 using std::cout;
@@ -12,6 +12,9 @@ using std::endl;
 using std::string;
 using std::uint32_t;
 
+#define DEBUG
+
+#define DB_NAME "rainbow.db"
 #define WORD_HELLO "rainbow-db> "
 #define WORD_BYE "BYE"
 #define SIZE_USERNAME 255
@@ -53,6 +56,8 @@ struct Cursor {
 };
 
 struct Table {
+    std::FILE *db_fd;
+    uint32_t db_length;
     PagesBuffer *pages_buffer;
     Cursor *cursor;
 };
@@ -89,12 +94,13 @@ PREPARE_RESULT prepare_statement(InputBuffer *input_buffer, Statement *statement
 HANDLE_RESULT exec_insert(Statement *statement);
 HANDLE_RESULT exec_select();
 void print_row(Row *row);
-void init_table(Table *&table);
+Table *db_open(const char *filename);
 uint8_t *get_page(uint32_t page_num);
+void db_close();
 
 int main(int argc, char *argv[])
 {
-    init_table(table);
+    table = db_open(DB_NAME);
     InputBuffer *input_buffer = new InputBuffer;
 
     while (true) {
@@ -149,6 +155,7 @@ HANDLE_RESULT handle_input(InputBuffer *input_buffer) {
 
 HANDLE_RESULT handle_dot_command(InputBuffer *input_buffer) {
     if (input_buffer->data == ".exit") {
+        db_close();
         cout << WORD_BYE;
         exit(EXIT_SUCCESS);
     } else {
@@ -244,27 +251,80 @@ HANDLE_RESULT exec_select() {
 }
 
 void print_row(Row *row) {
-    cout << "id:      \t" << row->id << endl;
-    cout << "++++++++++++++++++++++++++++++++++" << endl;
-    cout << "username:\t" << row->username << endl;
-    cout << "++++++++++++++++++++++++++++++++++" << endl;
-    cout << "email:   \t" << row->email << endl;
+    cout << "id:      \t" << row->id                << endl;
+    cout << "++++++++++++++++++++++++++++++++++"    << endl;
+    cout << "username:\t" << row->username          << endl;
+    cout << "++++++++++++++++++++++++++++++++++"    << endl;
+    cout << "email:   \t" << row->email             << endl;
 }
 
-void init_table(Table *&table) {
-    table = new Table;
+Table *db_open(const char *filename) {
+    Table *table = new Table;
+
+    table->db_fd = fopen(filename, "a+");
+    fseek(table->db_fd, 0, SEEK_END);
+    table->db_length = ftell(table->db_fd);
 
     table->pages_buffer = new PagesBuffer;
-    table->pages_buffer->num_pages = 0;
-
     table->cursor = new Cursor;
-    table->cursor->cur_page_num = 0;
-    table->cursor->cur_row_num = 0;
+    table->pages_buffer->num_pages = table->db_length / SIZE_PAGE;
+    table->cursor->cur_page_num = table->pages_buffer->num_pages-1;
+    uint32_t num_row_of_last_page = (table->db_length % SIZE_PAGE) / ROW_SIZE;
+    if (num_row_of_last_page) {
+        table->pages_buffer->num_pages++;
+        table->cursor->cur_page_num++;
+    }
+
+    table->cursor->cur_row_num = num_row_of_last_page;
+
+    return table;
 }
 
 uint8_t *get_page(uint32_t page_num) {
+    if (page_num > TABLE_MAX_PAGES) {
+        #ifdef DEBUG
+        cout << "get_page: page_num " << page_num << " out of bounds" << endl;
+        #endif
+        exit(EXIT_FAILURE);
+    }
+
     if (table->pages_buffer->pages[page_num] == nullptr) {
         table->pages_buffer->pages[page_num] = new uint8_t[SIZE_PAGE];
     }
+
+    if (page_num < table->pages_buffer->num_pages) {
+        fseek(table->db_fd, page_num*SIZE_PAGE, SEEK_SET);
+        fread(table->pages_buffer->pages[page_num], SIZE_PAGE, 1, table->db_fd);
+    }
+
+    if (page_num >= table->pages_buffer->num_pages)
+        table->pages_buffer->num_pages = page_num + 1;
+
     return table->pages_buffer->pages[page_num];
+}
+
+void db_close() {
+
+    for (uint32_t i = 0;i < table->cursor->cur_page_num;++i) {
+        if (table->pages_buffer->pages[i] != nullptr) {
+            fseek(table->db_fd, i*SIZE_PAGE, SEEK_SET);
+            fwrite(table->pages_buffer->pages[i], SIZE_PAGE, 1, table->db_fd);
+            delete [] table->pages_buffer->pages[i];
+            table->pages_buffer->pages[i] = nullptr;
+        }
+    }
+
+    if (table->pages_buffer->pages[table->cursor->cur_page_num] != nullptr) {
+        for (uint32_t i = 0;i < table->cursor->cur_row_num;++i) {
+            fseek(table->db_fd, table->cursor->cur_page_num*SIZE_PAGE+i*ROW_SIZE, SEEK_SET);
+            fwrite(table->pages_buffer->pages[table->cursor->cur_page_num]+i*ROW_SIZE, ROW_SIZE, 1, table->db_fd);
+        }
+        delete [] table->pages_buffer->pages[table->cursor->cur_page_num];
+        table->pages_buffer->pages[table->cursor->cur_page_num] = nullptr;
+    }
+
+    fclose(table->db_fd);
+    delete table->pages_buffer;
+    delete table->cursor;
+    delete table;
 }
